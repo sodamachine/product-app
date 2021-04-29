9a71fb29f3cd129de75f69936e1441b97093ad91
Stephanie <stephanie.ohn@gmail.com>
2021-04-28 20:44:45 -0400

    sets up index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,11 @@
 class ProductsController < ApplicationController
 
+    def index
+        @products = Product.all
+    end
+
+    def show
+        @product = Product.find_by(id: params[:id])
+    end
+
 end
