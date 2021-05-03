12fa279d6b365dd7b4182f129d8b2eb7420c9b9c
Stephanie <stephanie.ohn@gmail.com>
2021-05-03 01:18:03 -0400

    defines csessions controller action #create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate
+            session[:user_id] = @user_id
+            redirect_to user_products_path(@user)
+        else
+            render :new
+        end
     end
 
     def destroy
