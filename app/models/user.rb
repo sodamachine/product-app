8aabd9fae6924cf54dbfab543abb7b694c43e0ae
Stephanie <stephanie.ohn@gmail.com>
2021-05-02 23:59:00 -0400

    sets up user auth routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :solutions
     has_many :products, through: :solutions
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_015242) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
