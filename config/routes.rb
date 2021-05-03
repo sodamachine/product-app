ef0fab3b469a286b2d05daa106cd5b98a4ad1ac0
Stephanie <stephanie.ohn@gmail.com>
2021-05-02 22:27:52 -0400

    nests routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products
-  resources :solutions
-  
   resources :users do
     resources :products
+  end
+  
+  resources :products do
+    resources :solutions
   end
 
 end
