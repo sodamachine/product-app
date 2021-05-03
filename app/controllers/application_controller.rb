4f197c2c5429d7e188fdb2082f74e9603560564a
Stephanie <stephanie.ohn@gmail.com>
2021-05-02 22:05:10 -0400

    sets up users routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     helper method(:current_user)
 
     def current_user
+        @current_user = User.first
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :solutions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :products
   resources :solutions
+  resources :users
 
 end
